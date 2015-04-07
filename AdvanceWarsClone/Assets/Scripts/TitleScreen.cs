f8d0ad541af513f9a18ffe1c6940d8846e3690a7
Joshua Rand <JRand@student.dwc.edu>
2015-04-07 17:49:06 -0400

    Added Unity project with start of state machine

---
+++ AdvanceWarsClone/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+    public enum gameStates
+    {
+        titleScreen = 0,
+        mainGame
+    }
+
+    public gameStates currentState;
+    public gameStates prevState;
+
+    void Awake()
+    {
+        currentState = gameStates.titleScreen;
+        prevState = currentState;
+    }
+
+    void OnEnable()
+    {
+
+    }
+
+    void OnDisable()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeGameState(gameStates newState)
+    {
+        switch (newState)
+        {
+            case gameStates.titleScreen:
+                prevState = currentState;
+                currentState = gameStates.titleScreen;
+                break;
+
+            case gameStates.mainGame:
+                prevState = currentState;
+                currentState = gameStates.mainGame;
+                break;
+        }
+    }
+}
