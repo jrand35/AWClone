b92d7e8639ffa86a4e4298074252d1a211c5ac2d
Joshua Rand <JRand@student.dwc.edu>
2015-04-09 17:25:31 -0400

    Added states

---
+++ AdvanceWarsClone/Assets/Scripts/MenuStateController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuStateController : MonoBehaviour {
+
+    public enum menuStates
+    {
+        titleScreen = 0,
+        mapSelect
+    }
+
+    public delegate void MenuStateHandler(MenuStateController.menuStates newState);
+    public static event MenuStateHandler onStateChange;
+
+    void OnDisable()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	    //Change states
+	}
+}
---
+++ AdvanceWarsClone/Assets/Scripts/MenuStateListener.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuStateListener : MonoBehaviour {
+
+    private MenuStateController.menuStates currentState = MenuStateController.menuStates.titleScreen;
+    private MenuStateController.menuStates previousState = MenuStateController.menuStates.titleScreen;
+    public GameObject titleScreenObject;
+    public GameObject mapSelectObject;
+    private bool titleScreenActive;
+    private bool mapSelectActive;
+
+    void Awake()
+    {
+        titleScreenActive = false;
+        mapSelectActive = false;
+    }
+
+    void OnEnable()
+    {
+        MenuStateController.onStateChange += onStateChange;
+    }
+
+    void OnDisable()
+    {
+        MenuStateController.onStateChange -= onStateChange;
+    }
+
+    void LateUpdate()
+    {
+        onStateCycle();
+    }
+
+    void onStateCycle()
+    {
+        switch (currentState)
+        {
+            case MenuStateController.menuStates.titleScreen:
+                if (!titleScreenActive)
+                {
+                    titleScreenActive = true;
+                    mapSelectActive = false;
+                    titleScreenObject.SetActive(true);
+                }
+                break;
+
+            case MenuStateController.menuStates.mapSelect:
+                if (!mapSelectActive)
+                {
+                    titleScreenActive = false;
+                    mapSelectActive = true;
+                    mapSelectObject.SetActive(true);
+                }
+                break;
+        }
+    }
+
+    void onStateChange(MenuStateController.menuStates newState)
+    {
+        if (newState == currentState)
+            return;
+        switch (newState)
+        {
+            case MenuStateController.menuStates.titleScreen:
+                break;
+
+            case MenuStateController.menuStates.mapSelect:
+                break;
+        }
+        previousState = currentState;
+        currentState = newState;
+    }
+}
