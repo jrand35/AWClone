df9423b7122998847d50da2bf004e2eb18fd2e8e
Jraymond <Jraymond@student.dwc.edu>
2015-04-09 17:47:19 -0400

    test for terrain

---
+++ AdvanceWarsClone/Assets/UnitMovment.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitMovment : MonoBehaviour {
+
+
+    public GameObject unit;
+
+    private Transform unitTransform;
+
+
+	// Use this for initialization
+	void Start () {
+
+        unitTransform = unit.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        unitTransform = unit.transform;
+
+
+	
+	}
+
+    private void moveUnit(int distance)
+    {
+        unit.
+    }
+
+
+
+
+}
