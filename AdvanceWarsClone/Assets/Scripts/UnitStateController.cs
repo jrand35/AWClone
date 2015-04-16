093f076723f3aea54beb779c1c2ba90fe0eea13c
magpaoa <magpaoa@student.dwc.edu>
2015-04-16 16:44:43 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	AdvanceWarsClone/Library/ScriptAssemblies/Assembly-CSharp.dll
    	AdvanceWarsClone/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	AdvanceWarsClone/Library/assetDatabase3
    	AdvanceWarsClone/Library/guidmapper
    	AdvanceWarsClone/Library/metadata/cc/ccac59bf757b2c94a8c714b3d0e72110

---
+++ AdvanceWarsClone/Assets/Scripts/UnitStateController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitStateController : MonoBehaviour
+{
+
+    public enum unitStates
+    {
+        idle = 0,
+        inactive,
+        dead,
+        selected,
+        moving,
+        attacking
+    }
+
+    public delegate void UnitSateHandler(UnitStateController.unitStates newState, GameObject caller);
+    public static event UnitSateHandler onStateChange;
+
+    void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+        onStateChange(UnitStateController.unitStates.selected, gameObject);
+    }
+
+    //Possibly use for changing states
+    void LateUpdate()
+    {
+    }
+}
---
+++ AdvanceWarsClone/Assets/Scripts/UnitStateListener.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitStateListener : MonoBehaviour
+{
+
+    private Animator anim;
+    private UnitStateController.unitStates currentState = UnitStateController.unitStates.idle;
+    private UnitStateController.unitStates previousState = UnitStateController.unitStates.idle;
+
+    void Awake()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        UnitStateController.onStateChange += onStateChange;
+    }
+
+    void OnDisable()
+    {
+        UnitStateController.onStateChange -= onStateChange;
+    }
+
+    void LateUpdate()
+    {
+        onStateCycle();
+    }
+
+    void onStateCycle()
+    {
+        switch (currentState)
+        {
+            case UnitStateController.unitStates.idle:
+                break;
+
+            case UnitStateController.unitStates.inactive:
+                break;
+
+            case UnitStateController.unitStates.dead:
+                break;
+
+            case UnitStateController.unitStates.selected:
+                break;
+
+            case UnitStateController.unitStates.moving:
+                break;
+
+            case UnitStateController.unitStates.attacking:
+                break;
+        }
+    }
+
+    void onStateChange(UnitStateController.unitStates newState, GameObject caller)
+    {
+        if (newState == currentState)
+            return;
+
+        //When the state is first changed
+        switch (newState)
+        {
+            case UnitStateController.unitStates.idle:
+                //anim.SetBool("Idle", true);
+                break;
+
+            case UnitStateController.unitStates.inactive:
+                //anim.SetBool("Inactive", true);
+                break;
+
+            case UnitStateController.unitStates.dead:
+                Destroy(caller.gameObject);    //May change
+                break;
+
+            case UnitStateController.unitStates.selected:
+                caller.transform.position = new Vector3(-1000f, -1000f, -1000f);
+                onStateChange(UnitStateController.unitStates.idle, gameObject);
+                //anim.SetBool("Selected", true);
+                break;
+
+            case UnitStateController.unitStates.moving:
+                //anim.SetBool("Moving", true);
+                break;
+
+            case UnitStateController.unitStates.attacking:
+                //anim.SetBool("Attacking", true);
+                break;
+        }
+        previousState = currentState;
+        currentState = newState;
+    }
+}
