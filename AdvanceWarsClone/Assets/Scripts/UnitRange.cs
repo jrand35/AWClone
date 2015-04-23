3998c06d4d48c227299f019134898575208da936
Skywinder9 <Skywinder9@gmail.com>
2015-04-23 17:38:48 -0400

    added range Script
    this should work

---
+++ AdvanceWarsClone/Assets/Scripts/UnitRange.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitRange : MonoBehaviour {
+    public GameObject attachedUnit;
+    private string RedOrBlue;
+    public UnitStateListener unitScript;
+	// Use this for initialization
+	void Start () 
+    {
+        if (attachedUnit.tag == "blue")
+        {
+            RedOrBlue = "blue";
+        }
+        if (attachedUnit.tag == "red")
+        {
+            RedOrBlue = "red";
+        }
+	}
+    void OnTriggerExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag != RedOrBlue)
+        {
+            unitScript.canAttack = false;
+        }
+    }
+    void OnTriggerEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag != RedOrBlue)
+        {
+            unitScript.canAttack = true;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
