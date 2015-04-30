f6db7adeadbea82ecccb428573907f69d5384984
Jraymond <Jraymond@student.dwc.edu>
2015-04-30 15:56:17 -0400

    city scripts
    uploading city scripts

---
+++ AdvanceWarsClone/Assets/Scripts/cityhealth.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cityhealth : MonoBehaviour
+{
+
+    public int health;
+
+}
---
+++ AdvanceWarsClone/Assets/Scripts/cityscript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cityscript : MonoBehaviour {
+
+    private GameObject unit;
+    private bool occupied;
+
+    public GameObject city;
+    public int healAmount;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+    void OnTriggerEnter2D(Collider2D call)
+    {
+        unit = call.gameObject;
+        occupied = true;
+    }
+
+    void OnTriggerExit2D(Collider2D call)
+    {
+        unit = null;
+        occupied = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        if (unit.tag == this.tag && occupied == true)
+            heal();
+
+        else if (occupied == true)
+            capture();
+	
+	}
+
+    private void heal()
+    {
+        if (unit.GetComponent<UnitStatus>().Health <= 10)
+        {
+            if (unit.GetComponent<UnitStatus>().Health == 10)
+                unit.GetComponent<UnitStatus>().Health++;
+            else
+                unit.GetComponent<UnitStatus>().Health += 2;
+        }
+    }
+
+    private void capture()
+    {
+
+
+
+    }
+
+
+
+
+
+}
