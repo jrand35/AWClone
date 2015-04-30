b7a7a0274ce4950026e343e76dc842120a247f12
Jraymond <Jraymond@student.dwc.edu>
2015-04-30 16:50:36 -0400

    unknown error in city script
    sdfs

--- AdvanceWarsClone/Assets/Scripts/cityhealth.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class cityhealth : MonoBehaviour
-{
-
-    public int health;
-
-}
--- AdvanceWarsClone/Assets/Scripts/cityscript.cs
+++ AdvanceWarsClone/Assets/Scripts/cityscript.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class cityscript : MonoBehaviour {
 
     private GameObject unit;
-    private bool occupied;
+    public bool occupied;
+    public int cityHealth = 10;
 
-    public GameObject city;
+    public SpriteRenderer city;
+    public SpriteRenderer redcity;
+    public SpriteRenderer bluecity;
     public int healAmount;
 
 
@@ -19,6 +22,7 @@ public class cityscript : MonoBehaviour {
 	
     void OnTriggerEnter2D(Collider2D call)
     {
+        Debug.Log("enter trigger");
         unit = call.gameObject;
         occupied = true;
     }
@@ -27,18 +31,22 @@ public class cityscript : MonoBehaviour {
     {
         unit = null;
         occupied = false;
+        cityHealth = 10;
     }
 
 	// Update is called once per frame
 	void Update () {
 
+        if (occupied == true)
+        {
+            if (unit.tag == gameObject.tag)
+                heal();
+
+            else if (occupied == true)
+                capture();
+        }
+
         
-
-        if (unit.tag == this.tag && occupied == true)
-            heal();
-
-        else if (occupied == true)
-            capture();
 	
 	}
 
@@ -55,9 +63,26 @@ public class cityscript : MonoBehaviour {
 
     private void capture()
     {
-
-
-
+        if(unit.tag != this.tag)
+        {
+            this.cityHealth -= 5;
+            if(cityHealth <= 0)
+            {
+                if(unit.tag == "red")
+                {
+                    this.redcity.enabled = true;
+                    this.city.enabled = false;
+                    this.bluecity.enabled = false;
+                }
+                else
+                {
+                    this.redcity.enabled = false;
+                    this.city.enabled = false;
+                    this.bluecity.enabled = true;
+                }
+                cityHealth = 10;
+            }
+        }
     }
 
 
